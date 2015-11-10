8f85931e1ee3513fe4dc23ae330118cda176b2bd
kkloberdanz <kyle.g.kloberdanz@gmail.com>
2015-11-09 23:40:15 -0600

    Create binToDec.py

---
+++ binToDec.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import sys
+
+def assertAllBinary( numAsString ):
+    nums = []
+    nums = list(range(0, 2))
+    nums = str(nums)
+    for digit in numAsString:
+        if digit not in nums:
+            print("Non-binary input")
+            sys.exit()
+
+def binaryToDecimal( binAsString ):
+    factor = 1
+    total = 0
+    # reverseString = binAsString.reverse()
+    reverseString = binAsString[::-1]
+    for digit in reverseString:
+        total = total + (factor * int(digit))
+        factor *= 2
+    return total
+
+inputArg = sys.argv[1]
+assertAllBinary( inputArg ) 
+
+print( binaryToDecimal(inputArg) )
